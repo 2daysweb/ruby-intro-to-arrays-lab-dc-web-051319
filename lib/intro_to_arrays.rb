9de3261649f4597259684faf6d0f7851601329fb
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-23 20:19:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,7 +13,20 @@ def first_element(array)
 end
 
 def third_element(array)
-  return array[2]
+  return array[]
 end
 
-def
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.shift
+end
+
+def last_element_with_array_methods
+  return array.pop
+end
+
+def length_of_array
+  return 
